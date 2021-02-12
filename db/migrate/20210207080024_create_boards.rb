a998ebe836fc4b421313d2aaf757ac101dd44e7b
ShogoTasaki <mcfc0514@gmail.com>
2021-02-12 22:11:32 +0900

    Board画面途中

---
+++ db/migrate/20210207080024_create_boards.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateBoards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :boards do |t|
+      t.references :user, null: false
+      t.string :title, null: false
+      t.text :content, null: false
+      t.timestamps
+    end
+  end
+end
