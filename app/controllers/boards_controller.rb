3f0df5d4a0f71bde7de2dc5df31d08b3c57818a4
ShogoTasaki <mcfc0514@gmail.com>
2021-02-16 16:20:46 +0900

    day28　 edit, delete, new,createを追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,14 +10,43 @@ class BoardsController < ApplicationController
     @board = Board.find(params[:id])
   end
 
+  def new 
+    @board = current_user.boards.build
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to board_path(@board), notice: 'successfully saved'
+    else
+      flash.now[:error] = 'failed to save'
+      render :new
+    end
+  end
+
   def edit
     @board = current_user.boards.find(params[:id])
+  end
+
+  def update
+    @board = current_user.boards.find(params[:id])
+    if @board.update(board_params)
+      redirect_to board_path(@board), notice: 'successfully updated'
+    else
+      flash.now[:error] = 'failed to update'
+      render :edit
+    end
   end
 
   def destroy
     board = current_user.boards.find(params[:id])
     board.destroy
     redirect_to root_path, notice: 'Successfully deleted'
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
   end
 end
 
